1426855e8f28ded3e78841bb16b06084ba07aecd
Andreto <andreas.o.tornkvist@gmail.com>
2021-02-11 21:36:42 +0100

    initial code upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pewdipie-minecraft-profilepics
+# pewdiepie-minecraft-profilepics
+~~All~~ Most of Minecraft's blocks applied to Pewd's profile-pic.
---
+++ site_assets/index.js
@@ -0,0 +1,28 @@
+var root = document.documentElement;
+var imageContainer = document.getElementById('image-container');
+
+function generateImgs(key){
+  console.log(key);
+  imageContainer.innerHTML = "";
+  for (var i in filenames) {
+    if (filenames[i].includes(key)) {
+      let imgElem = document.createElement('img');
+      imgElem.src = 'high_res/' + filenames[i];
+      imgElem.classList.add('profile-img');
+
+      let aElem = document.createElement('a');
+      aElem.href = 'high_res/' + filenames[i];
+
+      aElem.appendChild(imgElem);
+      imageContainer.appendChild(aElem);
+    }
+  }
+}
+function setImgSize() {
+  let val = document.getElementById('img-size-range').value;
+  console.log(val + 'px');
+  root.style.setProperty('--img-size', val + 'px');
+}
+
+
+generateImgs("");
